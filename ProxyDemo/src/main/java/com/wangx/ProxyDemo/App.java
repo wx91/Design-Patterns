5ef602adb07b6da7ff3279ef02dc5dc48704cc56
wx91 <766221790@qq.com>
2016-05-12 09:52:15 +0800

    代理模式

---
+++ ProxyDemo/src/main/java/com/wangx/ProxyDemo/App.java
@@ -0,0 +1,13 @@
+package com.wangx.ProxyDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
