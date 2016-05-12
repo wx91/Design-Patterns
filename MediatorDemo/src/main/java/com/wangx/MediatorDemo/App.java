e7e86a4fff4bc6eeb955813314770b15301d5ea0
wx91 <766221790@qq.com>
2016-05-12 14:33:14 +0800

    场景

--- MediatorDemo/src/main/java/com/wangx/MediatorDemo/App.java
+++ MediatorDemo/src/main/java/com/wangx/MediatorDemo/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//采购人员采购电脑
+    	System.out.println("-----采购人员采购电脑-----");
+    	Purchase purcahse = new Purchase();
+    	purcahse.buyIBMComputer(100);
+    	//销售人员销售电脑
+    	System.out.println("\n-----销售人员销售电脑-----");
+    	Sale sale = new Sale();
+    	sale.sellIBMComputer(1);
+    	//库房管理人员管理库房
+    	System.out.println("\n-----库房管理人员清库处理-----");
+    	Stock stock = new Stock();
+    	stock.clearStock();
     }
 }
