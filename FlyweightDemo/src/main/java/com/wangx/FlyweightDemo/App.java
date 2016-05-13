f269066a5628931ecacc995364cdfc64b916f592
wx91 <766221790@qq.com>
2016-05-13 15:35:40 +0800

    享元模式

---
+++ FlyweightDemo/src/main/java/com/wangx/FlyweightDemo/App.java
@@ -0,0 +1,13 @@
+package com.wangx.FlyweightDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
