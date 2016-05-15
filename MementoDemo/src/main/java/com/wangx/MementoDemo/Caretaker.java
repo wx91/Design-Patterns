98b32a3a0af039fff4f3cd8d64b3482532233aee
wx91 <766221790@qq.com>
2016-05-15 12:13:57 +0800

    备忘录模式。

---
+++ MementoDemo/src/main/java/com/wangx/MementoDemo/Caretaker.java
@@ -0,0 +1,16 @@
+package com.wangx.MementoDemo;
+
+public class Caretaker {
+	//备忘录对象
+	private Memento memento;
+
+	public Memento getMemento() {
+		return memento;
+	}
+
+	public void setMemento(Memento memento) {
+		this.memento = memento;
+	}
+	
+
+}
